9d86fe6bccf3287e7a8d43dbc404217a82be81d0
Galadril <heinis.mark@gmail.com>
2020-08-20 20:33:42 +0200

    Working on the plugin for Domoticz (WLANThermo)

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# Domoticz-WLANThermo-Plugin
+
+WLANThermo Plugin for Domoticz Home Automation
+This plugin allows to receive the channel temperatures from WLANThermo within Domoticz 
+
+
+## Installation
+
+Python version 3.4 or higher required & Domoticz version 3.87xx or greater.
+
+To install:
+* Go in your Domoticz directory using a command line and open the plugins directory.
+* Run: ```git clone https://github.com/galadril/Domoticz-WLANThermo-Plugin.git```
+* Restart Domoticz.
+
+In the web UI, navigate to the Hardware page.  In the hardware dropdown there will be an entry called "WLANThermo".
+
+
+## Updating
+
+To update:
+* Go in your Domoticz directory using a command line and open the plugins directory then the Domoticz-WLANThermo-Plugin directory.
+* Run: ```git pull```
+* Restart Domoticz.
+
+
+## Change log
+
+| Version | Information |
+| ----- | ---------- |
+| 1.0.0 | Initial upload version |
---
+++ plugin.py
@@ -0,0 +1,242 @@
+#           Yi Hack Plugin
+#
+#           Author:     galadril, 2020
+#
+"""
+<plugin key="WLANThermo" name="WLANThermo" author="galadril" version="0.0.1" wikilink="https://github.com/galadril/Domoticz-WLANThermo-Plugin" externallink="">
+    <description>
+        <h2>WLANThermo Plugin</h2><br/>
+        <h3>Features</h3>
+        <ul style="list-style-type:square">
+            <li>Shows all the channels of WLANThermo within Domoticz</li>
+        </ul>
+        <h3>Devices</h3>
+        <ul style="list-style-type:square">
+            <li>Channel, Current Temperatures</li>
+            <li>Channel, SetPoint for Min Temperature</li>
+            <li>Channel, SetPoint for Max Temperature</li>
+            <li>Pitmaster, Percentage of fan speed</li>
+            <li>Pitmaster, Set mode (off/auto/manual)</li>
+        </ul>
+    </description>
+    <params>
+        <param field="Address" label="IP Address" width="200px" required="true" default="0.0.0.0"/>
+        <param field="Mode6" label="Debug" width="200px">
+            <options>
+                <option label="None" value="0"  default="true" />
+                <option label="Python Only" value="2"/>
+                <option label="Basic Debugging" value="62"/>
+                <option label="Basic + Messages" value="126"/>
+                <option label="Connections Only" value="16"/>
+                <option label="Connections + Queue" value="144"/>
+                <option label="All" value="-1"/>
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+import Domoticz
+import sys
+import json
+import base64
+
+class BasePlugin:
+    WLANThermoConn = None
+    nextConnect = 1
+    outstandingPings = 0
+    
+    sendData = { 'Verb' : 'GET', 'URL'  : '/data'}
+    sendAfterConnect = { 'Verb' : 'GET', 'URL'  : '/data'}
+    
+    encoded_credentials = ''
+    basicAuth = ''
+        
+    def onStart(self):
+        if Parameters["Mode6"] != "0":
+            Domoticz.Debugging(int(Parameters["Mode6"]))
+            DumpConfigToLog()
+        
+        sendData = { 'Verb' : 'GET', 'URL'  : '/data'}
+        sendAfterConnect = sendData
+        
+        self.WLANThermoConn = Domoticz.Connection(Name="WLANThermoConn", Transport="TCP/IP", Protocol="HTTP", Address=Parameters["Address"], Port="80")
+        self.WLANThermoConn.Connect()
+            
+        Domoticz.Heartbeat(10)
+        return True
+        
+    def onConnect(self, Connection, Status, Description):
+        if (Status == 0):
+            Domoticz.Log("Connected successfully to: "+Connection.Address+":"+Connection.Port)
+            self.WLANThermoConn.Send(self.sendAfterConnect)
+        else:
+            Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port)
+            Domoticz.Debug("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port+" with error: "+Description)
+            for Key in Devices:
+                UpdateDevice(Key, 0, Devices[Key].sValue, 1)
+        return True
+
+    def onMessage(self, Connection, Data):
+        Response = json.loads(Data["Data"])
+        DumpJSONResponseToLog(Response)
+        
+        for channel in Response["channel"]:
+            if channel['temp'] < 999.0:
+                Domoticz.Log("Receive new temperature for channel: " + str(channel['name']) + " | " +  str(channel['temp']) )
+                
+                unitId = int(channel['number'])
+                unitIdMin = int(channel['number'])+10
+                unitIdMax = int(channel['number'])+100
+                
+                temp = int(channel['temp'])
+                min = int(channel['min'])
+                max = int(channel['max'])
+                
+                if unitId not in Devices:
+                    Domoticz.Device(Name=channel['name'], Unit=unitId, TypeName="Temperature").Create()
+                if unitIdMin not in Devices:
+                    Domoticz.Device(Name=channel['name']+ " Min", Unit=unitIdMin, Type=242, Subtype=1).Create()
+                if unitIdMax not in Devices:
+                    Domoticz.Device(Name=channel['name']+ " Max", Unit=unitIdMax, Type=242, Subtype=1).Create()
+                
+                UpdateDevice(unitId, temp, str(temp), TimedOut=0)
+                UpdateDevice(unitIdMin, min, str(min), TimedOut=0)
+                UpdateDevice(unitIdMax, max, str(max), TimedOut=0)
+            
+        return True
+
+    def onCommand(self, Unit, Command, Level, Hue):
+        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level) + ", Connected: " + str(self.WLANThermoConn.Connected()))
+       
+        return True
+
+    def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
+        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+        return
+
+    def onHeartbeat(self):
+        try:
+            if (self.WLANThermoConn.Connected()):
+                if (self.outstandingPings > 3):
+                    self.WLANThermoConn.Disconnect()
+                    self.nextConnect = 0
+                else:
+                    self.WLANThermoConn.Send(self.sendData)
+                    self.outstandingPings = self.outstandingPings + 1
+            else:
+                # if not connected try and reconnected every 3 heartbeats
+                self.outstandingPings = 0
+                self.nextConnect = self.nextConnect - 1
+                self.sendAfterConnect = self.sendData
+                if (self.nextConnect <= 0):
+                    self.nextConnect = 1
+                    self.WLANThermoConn.Connect()
+            return True
+        except:
+            Domoticz.Log("Unhandled exception in onHeartbeat, forcing disconnect.")
+            self.onDisconnect(self.WLANThermoConn)
+            self.WLANThermoConn = None
+        
+    def onDisconnect(self, Connection):
+        Domoticz.Log("Device has disconnected")
+        return
+
+    def onStop(self):
+        Domoticz.Log("onStop called")
+        return True
+
+    def TurnOn(self):
+        self.WLANThermoConn.Send(self.sendOnAction)
+        return
+
+    def TurnOff(self):
+        self.WLANThermoConn.Send(self.sendOffAction)
+        return
+
+    def ClearDevices(self):
+        # Stop everything and make sure things are synced
+        self.cameraState = 0
+        self.SyncDevices(0)
+        return
+        
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onConnect(Connection, Status, Description):
+    global _plugin
+    _plugin.onConnect(Connection, Status, Description)
+
+def onMessage(Connection, Data):
+    global _plugin
+    _plugin.onMessage(Connection, Data)
+
+def onCommand(Unit, Command, Level, Hue):
+    global _plugin
+    _plugin.onCommand(Unit, Command, Level, Hue)
+
+def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
+    global _plugin
+    _plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
+
+def onDisconnect(Connection):
+    global _plugin
+    _plugin.onDisconnect(Connection)
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+# Generic helper functions
+def DumpConfigToLog():
+    for x in Parameters:
+        if Parameters[x] != "":
+            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+    Domoticz.Debug("Settings count: " + str(len(Settings)))
+    for x in Settings:
+        Domoticz.Debug( "'" + x + "':'" + str(Settings[x]) + "'")
+    for x in Images:
+        Domoticz.Debug( "'" + x + "':'" + str(Images[x]) + "'")
+    Domoticz.Debug("Device count: " + str(len(Devices)))
+    for x in Devices:
+        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
+        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
+        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
+        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
+        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+    return
+
+def DumpJSONResponseToLog(jsonDict):
+    if isinstance(jsonDict, dict):
+        Domoticz.Log("JSON Response Details ("+str(len(jsonDict))+"):")
+        for x in jsonDict:
+            if isinstance(jsonDict[x], dict):
+                Domoticz.Log("--->'"+x+" ("+str(len(jsonDict[x]))+"):")
+                for y in jsonDict[x]:
+                    Domoticz.Log("------->'" + y + "':'" + str(jsonDict[x][y]) + "'")
+            else:
+                Domoticz.Log("--->'" + x + "':'" + str(jsonDict[x]) + "'")
+
+def UpdateDevice(Unit, nValue, sValue, TimedOut):
+    # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
+    if (Unit in Devices):
+        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue) or (Devices[Unit].TimedOut != TimedOut):
+            Devices[Unit].Update(nValue=nValue, sValue=str(sValue), TimedOut=TimedOut)
+            Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
+    return
+
+# Synchronise images to match parameter in hardware page
+def UpdateImage(Unit):
+    if (Unit in Devices) and (Parameters["Mode1"] in Images):
+        Domoticz.Debug("Device Image update: '" + Parameters["Mode1"] + "', Currently "+str(Devices[Unit].Image)+", should be "+str( Images[Parameters["Mode1"]].ID))
+        if (Devices[Unit].Image != Images[Parameters["Mode1"]].ID):
+            Devices[Unit].Update(nValue=Devices[Unit].nValue, sValue=str(Devices[Unit].sValue), Image=Images[Parameters["Mode1"]].ID)
+    return
